02fea2f3374808df0b885a47cda04afcabf29417
amber bond <abond0017@gmail.com>
2020-05-28 18:09:42 -0400

    second commit_delete submit_added alerts

---
+++ functions.js
@@ -0,0 +1,22 @@
+
+function subjectsRadio1() {
+
+    var subjects=new Array();
+subjects[0]='Copy and paste links into your browser';
+subjects[1]='math-link';
+subjects[2]='math-link';
+subjects[3]='math-link';
+
+
+alert(subjects.join("\n"));
+}
+
+function gradesRadio() {
+    var grades=new Array();
+    grades[0]='Copy and paste links into your browser';
+    grades[1]='preschool-link';
+    grades[2]='preschool-link';
+    grades[3]='preschool-link';
+
+    alert(grades.join("\n"));
+}
